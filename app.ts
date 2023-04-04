4913bb7d3a39df968129ee25737371fad4080b9e
kyahn23 <kyahn23@gmail.com>
2023-04-05 02:51:39 +0900

    enum

--- app.ts
+++ app.ts
@@ -1,17 +1,40 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "kyahn",
+//   age: 30,
+//   hobbies: ["Sports", "Game"],
+//   role: [2, "author"],
+// };
+
+// // person.role.push("Admin");
+// // person.role[1] = 10;
+
+// // person.role = [0, "admin", "user"];
+// console.log(person);
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "kyahn",
   age: 30,
   hobbies: ["Sports", "Game"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-// person.role.push("Admin");
-// person.role[1] = 10;
-
-// person.role = [0, "admin", "user"];
 console.log(person);
+
+if (person.role === Role.ADMIN) {
+  console.log(Role.ADMIN);
+}
