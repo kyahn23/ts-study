4c81e0fd81d6d9ad8a0e2f299564a9d599992329
kyahn23 <kyahn23@gmail.com>
2023-04-14 03:28:35 +0900

    type alias

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+// type alias
+
 function combine(
-  input1: number | string,
-  input2: number | string,
+  input1: Combinable,
+  input2: Combinable,
   // resultConvserion: string
-  resultConvserion: "as-number" | "as-text"
+  resultConvserion: ConversionDescriptor
 ) {
   let result;
 
@@ -31,4 +35,12 @@ console.log(combinedStringAges);
 
 const combinedNames = combine("ky", "ahn", "as-text");
 console.log(combinedNames);
-``;
+
+type User = { name: string; age: number };
+const u1: User = { name: "kyahn", age: 35 };
+
+function test1(user: User) {
+  console.log(`Hi, I'm ` + user.name, `I'm ` + user.age + ` years old`);
+}
+
+test1({ name: "kevin", age: 30 });
