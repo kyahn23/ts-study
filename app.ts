110896995448648b639d38e77617a34d08236d21
kyahn23 <kyahn23@gmail.com>
2023-06-08 03:28:06 +0900

    spread, arg

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-const add = (a: number, b: number) => {
-  return a + b;
-};
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
 
-console.log(add(2, 3));
+// console.log(add(2, 3));
 
 const func = (arr: any[]) => {
   return arr.reduce((acm, cur) => {
@@ -23,3 +23,29 @@ const button = document.querySelector("button");
 if (button) {
   button.addEventListener("click", (event) => console.log(event));
 }
+
+const hobbies = ["Sports", "Cooking"];
+
+console.log([...hobbies, "Game"]);
+console.log(["Game", ...hobbies]);
+
+const person = {
+  name: "kyahn",
+  age: 30,
+};
+
+const copiedPerson = { ...person, height: 180 };
+
+console.log(copiedPerson);
+
+// 여러값을 매개변수로 받을 경우
+const add = (...numbers: number[]) => {
+  // const add = (...numbers: [number,number,number]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addNumbers = add(1.1, 2, 3, 4, 5);
+
+console.log(addNumbers);
